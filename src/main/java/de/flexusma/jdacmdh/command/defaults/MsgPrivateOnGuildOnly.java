e7cc858b9c3e3d2731f314b3f2f6cec2019af924
Flexusma <luregh@gmail.com>
2020-05-09 22:57:41 +0200

    Release v1.1.12 | New Bugfixes
    Updated license information

--- src/main/java/de/flexusma/jdacmdh/command/defaults/MsgPrivateOnGuildOnly.java
+++ src/main/java/de/flexusma/jdacmdh/command/defaults/MsgPrivateOnGuildOnly.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2019-2020 Lucas Regh | Flexusma
+ * This Project is licensed under
+ * MPL-2.0
+ * A copy of the complete license can be found in the root folder of this project in a file called License
+ */
 package de.flexusma.jdacmdh.command.defaults;
 
 import de.flexusma.jdacmdh.command.Command;
